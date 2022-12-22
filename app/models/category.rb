bd1293c461be1fb5aacd3d478df50b7b82f5f045
mayumitanaka <118504619+mayumitanaka@users.noreply.github.com>
2022-12-22 14:16:05 +0900

    ActiveHashの導入

---
+++ app/models/category.rb
@@ -0,0 +1,16 @@
+class category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '主菜' },
+    { id: 3, name: '副菜' },
+    { id: 4, name: '汁物' },
+    { id: 5, name: '野菜料理' },
+    { id: 6, name: '肉料理' },
+    { id: 7, name: '卵料理' },
+    { id: 8, name: '魚料理' },
+    { id: 9, name: '豆料理' },
+  ]
+   include ActiveHash::Associations
+   has_many :recipes
+  end
+  
