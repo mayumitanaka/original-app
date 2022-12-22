9b0f0a61ab1a8c292fa45e8eec3d052f9b16f610
mayumitanaka <118504619+mayumitanaka@users.noreply.github.com>
2022-12-22 19:36:42 +0900

    recipeモデルの単体テストコード実装

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,10 +2,12 @@ class Recipe < ApplicationRecord
   with_options presence: true do
     validates :recipe_title
     validates :recipe_procedure
-    validates :recipe_volume, format: { with: /\A\d{1,2}\z/, message: 'は半角数字を入力してください', allow_blank: true }
+    validates :recipe_volume, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 99,
+                                              allow_blank: true }, format: { with: /\d/, allow_blank: true }
     validates :recipe_material
     validates :recipe_quantity
-    validates :cooking_time, format: { with: /\A\d{1,3}\z/, message: 'は半角数字を入力してください', allow_blank: true }
+    validates :cooking_time, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 999,
+                                             allow_blank: true }, format: { with: /\d/, allow_blank: true }
     validates :tool_id, numericality: { other_than: 1, message: 'を入力してください' }
     validates :category_id, numericality: { other_than: 1, message: 'を入力してください' }
   end
