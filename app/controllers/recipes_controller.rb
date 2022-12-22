437a16ac362bd1c8f8dc829258d8114446af7e41
mayumitanaka <118504619+mayumitanaka@users.noreply.github.com>
2022-12-22 18:24:50 +0900

    createアクションの設定

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,5 +5,21 @@ class RecipesController < ApplicationController
   end
 
   def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to root_path
+    else
+      render :new
+    end    
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:recipe_title, :recipe_procedure, :recipe_volume, :recipe_material, :recipe_quantity,
+                                   :cooking_time, :tool_id, :category_id, :image).merge(user_id: current_user.id)
   end
 end
