efd9a2a221629a9772931c2c1b5cbabdaca82619
mayumitanaka <118504619+mayumitanaka@users.noreply.github.com>
2022-12-22 12:41:02 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :tool_id, numericality: { other_than: 1, message: 'を入力してください' }
+
+  has_many :recipes, dependent: :destroy
+  has_many :menus,   dependent: :destroy
+  has_many :foods,   dependent: :destroy
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :tool
 end
