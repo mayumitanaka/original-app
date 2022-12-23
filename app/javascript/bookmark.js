d4c6f67aa6c2ee44d07a66b11dcd859f3bf56abb
mayumitanaka <118504619+mayumitanaka@users.noreply.github.com>
2022-12-23 17:22:15 +0900

    お気に入りボタンのクリック機能実装

--- app/javascript/bookmark.js
+++ app/javascript/bookmark.js
@@ -1,3 +1,16 @@
-window.addEventListener('load', () => {
-  
-});
+function bookmarkBtn() {
+  const bookmark = document.getElementById('bookmark-btn');
+  const markDone = "登録済み"
+  const markRemove = "お気に入り"
+  bookmark.addEventListener("click", () => {
+    if (bookmark.getAttribute("style") == "background-color:#FFBEDA;") {
+      bookmark.removeAttribute("style")
+      bookmark.innerHTML = markRemove
+    } else {
+      bookmark.setAttribute("style", "background-color:#FFBEDA;")
+      bookmark.innerHTML = markDone
+    }
+  })
+}
+
+window.addEventListener('load', bookmarkBtn)
