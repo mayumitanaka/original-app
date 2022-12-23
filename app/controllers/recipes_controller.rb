8c6bdcd1f9073455d7de4c925b93b130ba236d5a
mayumitanaka <118504619+mayumitanaka@users.noreply.github.com>
2022-12-23 11:12:58 +0900

    indexアクションの設定

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,7 @@ class RecipesController < ApplicationController
   before_action :authenticate_user!, only: :new
 
   def index
+    @recipes = Recipe.includes(:user).order("created_at DESC")
   end
 
   def new
