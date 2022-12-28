bd334d34c18cdaeef83cf35531739e2e8bd258f7
mayumitanaka <118504619+mayumitanaka@users.noreply.github.com>
2022-12-28 21:51:13 +0900

    ヘッダーの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A[a-z\d]+\z/i.freeze
 
   with_options presence: true do
-    validates_format_of :password, with: PASSWORD_REGEX
+    validates_format_of :password, with: PASSWORD_REGEX, allow_blank: true
   end
 
   validates :nickname, presence: true
