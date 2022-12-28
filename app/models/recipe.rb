2ae4289afdf838646d739b0e715a8d4fd1115af0
mayumitanaka <118504619+mayumitanaka@users.noreply.github.com>
2022-12-29 01:09:44 +0900

    レシピ投稿ページの修正

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,12 +1,13 @@
 class Recipe < ApplicationRecord
   with_options presence: true do
+    validates :image
     validates :recipe_title
-    validates :recipe_procedure
     validates :recipe_volume, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 99,
                                               allow_blank: true }, format: { with: /\d/, allow_blank: true }
     validates :cooking_time, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 999,
                                              allow_blank: true }, format: { with: /\d/, allow_blank: true }
     validates :category_id, numericality: { other_than: 1, message: 'を入力してください' }
+    validates :recipe_procedure
   end
 
   belongs_to :user
--- db/migrate/20221227070129_create_ingredients.rb
+++ db/migrate/20221227070129_create_ingredients.rb
@@ -1,7 +1,7 @@
 class CreateIngredients < ActiveRecord::Migration[6.0]
   def change
     create_table :ingredients do |t|
-      t.string     :ing,   null: false
+      t.string     :ing,      null: false
       t.string     :quantity, null: false 
       t.references :recipe,   null: false,  foreign_key: true
       t.timestamps
