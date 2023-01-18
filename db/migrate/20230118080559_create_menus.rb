fb1519dc586a75ba27e5976de99f837d30486054
mayumitanaka <118504619+mayumitanaka@users.noreply.github.com>
2023-01-18 17:08:41 +0900

    menusテーブルの作成

---
+++ db/migrate/20230118080559_create_menus.rb
@@ -0,0 +1,9 @@
+class CreateMenus < ActiveRecord::Migration[6.0]
+  def change
+    create_table :menus do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :recipe, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
