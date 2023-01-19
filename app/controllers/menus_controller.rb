a772e95aca9f7e4335296843c241d09141988693
mayumitanaka <118504619+mayumitanaka@users.noreply.github.com>
2023-01-20 01:18:40 +0900

    index、new　仮ビューページの設定

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,13 +1,23 @@
 class MenusController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_menu, only: [:index, :new, :create, :edit, :update]
 
   def index
+    # current_user_idのものだけ表示する
+    @recipes = Recipe.includes(:user)
   end
 
   def new
+    @menu = Menu.new
   end
 
   def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      redirect_to menus_path
+    else
+      render :new
+    end
   end
 
   def edit    
@@ -19,4 +29,15 @@ class MenusController < ApplicationController
   def destroy
   end
 
+  private
+
+  def menu_params
+    params.require(:menu).permit(:recipe_id).merge(user_id: current_user.id)
+  end
+
+  def set_menu
+    @menu_main = Recipe.where(category_menu_id: 1)
+    @menu_sub = Recipe.where(category_menu_id: 2)
+    @menu_soup = Recipe.where(category_menu_id: 3)
+  end
 end
